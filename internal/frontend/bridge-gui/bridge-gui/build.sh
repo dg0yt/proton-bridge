ffafc3a61c519e2ea0fa0fed3d90ce093a4ae0db
Kai Pastor <dg0yt@darc.de>
2024-04-04 14:03:20 +0200

    Update build.sh

--- internal/frontend/bridge-gui/bridge-gui/build.sh
+++ internal/frontend/bridge-gui/bridge-gui/build.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Proton Mail Bridge. If not, see <https://www.gnu.org/licenses/>.
 
-if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]] ; then
+if true || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]] ; then
     Powershell.exe -File build.ps1 "$@"
     exit $?
 fi
